1bc20fffe84f7defcffecbb39507c0c108efec93
galatolofederico <galatolo.federico@gmail.com>
2018-07-10 18:45:24 +0200

    Added install from directory

--- cmd/install.js
+++ cmd/install.js
@@ -2,17 +2,26 @@ const repos = require("../lib/repos.js")
 const apps = require("../lib/apps.js")
 
 function installCommand(appname){
-    repos.getApp(appname).then(app => {
-        apps.installApp(app).then( () => {
-            console.log("App "+appname+" installed!")
-        }).catch(e => {
-            console.log("There was an error: "+e)
-            apps.uninstallApp(app)
+    const args = require("minimist")(Array.prototype.slice.call(arguments))
+    console.log(args)
+    if(args.d){
+        apps.installDirectory(args.d).then(app => {
+            console.log("DockerApp "+app.name+" installed!")
         })
-    }).catch(e => {
-        if(e) console.log("There was an error: "+e)
-        else console.log("App "+appname+" not found!")
-    })
+    } else {
+        let appname = args._[0]
+        repos.getApp(appname).then(app => {
+            apps.installApp(app).then( () => {
+                console.log("DockerApp "+appname+" installed!")
+            }).catch(e => {
+                console.log("There was an error: "+e)
+                apps.uninstallApp(app)
+            })
+        }).catch(e => {
+            if(e) console.log("There was an error: "+e)
+            else console.log("DockerApp "+appname+" not found!")
+        })
+    }
 }
 
 module.exports = installCommand
