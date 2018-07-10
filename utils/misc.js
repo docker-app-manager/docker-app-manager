b3107476fd7524e94083c69d660520c9e877bd06
galatolofederico <galatolo.federico@gmail.com>
2018-07-10 19:19:09 +0200

    use fs-extra instead of fs

--- utils/misc.js
+++ utils/misc.js
@@ -1,22 +1,5 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs-extra")
 const execSync = require('child_process').execSync
-
-
-function rimraf(dir_path) {
-    if (fs.existsSync(dir_path)) {
-        fs.readdirSync(dir_path).forEach(entry => {
-            var entry_path = path.join(dir_path, entry)
-            if (fs.lstatSync(entry_path).isDirectory()) {
-                rimraf(entry_path)
-            } else {
-                fs.unlinkSync(entry_path)
-            }
-        })
-        fs.rmdirSync(dir_path)
-    }
-}
-
 
 function installDesktopFile(file){
     let filesplit = file.split("/")
@@ -33,9 +16,7 @@ function uninstallDesktopFile(appname){
 }
 
 
-
 module.exports = {
-    rimraf: rimraf,
     installDesktopFile: installDesktopFile,
     uninstallDesktopFile: uninstallDesktopFile
 }
