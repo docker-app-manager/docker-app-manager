7f3fe439c25b175448837d358afd264cf1d8ba4c
galatolofederico <galatolo.federico@gmail.com>
2018-07-10 19:24:57 +0200

    New prototype for run function

--- cmd/run.js
+++ cmd/run.js
@@ -2,11 +2,12 @@ const repos = require("../lib/repos.js")
 const apps = require("../lib/apps.js")
 
 function runCommand(appname){
-    repos.getApp(appname).then(app => {
-        apps.runApp(app)
-    }).catch(e => {
+    try{
+        apps.runApp(appname)
+    }
+    catch(e) {
         console.log("There was an error: "+e)
-    })
+    }
 }
 
 module.exports = runCommand
